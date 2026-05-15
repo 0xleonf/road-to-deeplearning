a942a1861597a2700249293eaefc470a645ac933
0xleonf <ucihasaskee07@gmail.com>
2026-05-15 08:05:33 +0800

    example type-hint

--- chapter1/function.py
+++ chapter1/function.py
@@ -37,3 +37,19 @@ a = np.array([[1, 2, 3],
               [4, 5, 6]])
 b = np.array([10, 20, 30])
 print("a+b:\n", a+b)
+print()
+
+
+# type-hint
+def square(x: np.ndarray) -> np.ndarray:
+    '''
+    Square each element
+    '''
+    return np.power(x, 2)
+
+
+def leaky_relu(x: np.ndarray) -> np.ndarray:
+    '''
+    Apply "Leaky ReLU function to each element in ndarray"
+    '''
+    return np.maximum(0.2 * x, x)
