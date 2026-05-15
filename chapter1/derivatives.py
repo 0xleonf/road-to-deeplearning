7cffda875a224b72e7f570cc5da76fb12994d23c
0xleonf <ucihasaskee07@gmail.com>
2026-05-15 08:05:21 +0800

    derivatives?

---
+++ chapter1/derivatives.py
@@ -0,0 +1,19 @@
+from typing import Callable
+import numpy as np
+
+
+def equation(x: np.ndarray) -> np.ndarray:
+    return x ** 2
+
+
+def deriv(func: Callable[[np.ndarray], np.ndarray], input_: np.ndarray, delta: float = 0.001) -> np.ndarray:
+    '''
+    Evaluates the derivative of a function "func" at every element in the "input_" array
+    '''
+    return (func(input_ + delta) - func(input_ - delta)) / (2 * delta)
+
+
+input_ = np.array([3.0])
+result = deriv(equation, input_)
+
+print(f'Turunan di titik {input_} adalah: {result}')
